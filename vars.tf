6887b75437a270aea8ccf30cfe72d0d780196cf8
The-NOG <matthew.s.sponaugle@gmail.com>
2023-10-23 22:57:40 -0400

    cleanup

--- vars.tf
+++ vars.tf
@@ -1,13 +1,6 @@
 variable "glpi_db_user" {}
-variable "glpi_db_pass" {}
 variable "glpi_db_schema" {}
 variable "host" {}
 variable "domain" {}
 variable "tld" {}
 variable "sshkey" {}
-variable "ansible_user"{}
-variable "ansible_public_key" {}
-variable "glpi_version"{}
-variable "bootstraprepo"{}
-variable "bootstrapdir"{}
-variable "puppetrepo" {}
